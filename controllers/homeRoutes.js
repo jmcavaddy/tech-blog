8207082eeb8c9e2a4adfe9128b17a68c4e71de1f
jmcavaddy <jakemcavaddy@gmail.com>
2023-05-08 21:08:30 -0400

    Added handlebars for dashboard and homepage; added routes to serve dashboard and homepage handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,29 @@ const router = require('express').Router();
 const { Post, Comment, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+    try {
+      res.render('homepage', { 
 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }   
+});
+
+router.get('/login', (req, res) => {
+
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+
+    res.render('signup');
+});
+    
+router.get('/dashboard', (req, res) => {
+
+    res.render('dashboard');
+});
 
 module.exports = router;
