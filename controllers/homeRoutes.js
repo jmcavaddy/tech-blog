b229e4643c03db6812e4deb4bca0b24302790970
jmcavaddy <jakemcavaddy@gmail.com>
2023-05-09 09:03:17 -0400

    Added GET route for dashboard with authorization functionality; started adding content to dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -22,9 +22,24 @@ router.get('/signup', (req, res) => {
     res.render('signup');
 });
     
-router.get('/dashboard', (req, res) => {
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      // include: [{ model: Project }],
+    });
 
-    res.render('dashboard');
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
