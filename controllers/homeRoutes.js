b393bbe3f60a0ffab46c1a76072d3638dd09bdf6
jmcavaddy <jakemcavaddy@gmail.com>
2023-05-09 20:39:20 -0400

    Added posts to homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,13 +4,28 @@ const withAuth = require('../utils/auth');
 
 // GET homepage
 router.get('/', async (req, res) => {
-    try {
-      res.render('homepage', { 
+  try {
+    // Get all post and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
 
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }   
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // GET Login Page
